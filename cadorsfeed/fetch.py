e66e2f19611e4f7bca9be400b13238e249b1b3d2
Kurt Raschke <kurt@kurtraschke.com>
2010-08-23 22:13:10 -0400

    Raise exceptions for common errors rather than returning invalid data.

--- cadorsfeed/fetch.py
+++ cadorsfeed/fetch.py
@@ -1,3 +1,4 @@
+from werkzeug.exceptions import NotFound, InternalServerError
 import mechanize
 import re
 
@@ -16,7 +17,13 @@ def fetchReport(reportDate):
     br.select_form(name="pageForm")
     br["txt_ReportDate"] = reportDate
     response2 = br.submit(name="btn_SearchTop")
+    print response2.geturl()
+    if not response2.geturl().startswith("http://wwwapps.tc.gc.ca/Saf-Sec-Sur/2/cadors-screaq/rpt.aspx"):
+        raise InternalServerError()
     data = response2.get_data()
+    if re.search("There were no results for the search criteria you entered",
+                 data):
+        raise NotFound()        
     data_filtered = re.sub("""<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="[A-Za-z0-9/+=]*" />""",
                            "<!-- viewstate field stripped -->",
                            data)
