f55f9403a4b50b4f53c7ec48aaf6f5601d0f5a47
Kurt Raschke <kurt@kurtraschke.com>
2010-09-04 19:49:01 -0400

    Add support for static files.

--- cadorsfeed/application.py
+++ cadorsfeed/application.py
@@ -1,4 +1,5 @@
-from werkzeug import Request, ClosingIterator
+from os import path
+from werkzeug import Request, ClosingIterator, SharedDataMiddleware
 from werkzeug.exceptions import HTTPException
 
 import redis
@@ -7,6 +8,7 @@ from cadorsfeed.utils import local, local_manager, url_map
 from cadorsfeed import views
 from cadorsfeed.parse import make_transform
 
+STATIC_PATH = path.join(path.dirname(__file__), 'static')
 
 class CadorsFeed(object):
 
@@ -17,8 +19,11 @@ class CadorsFeed(object):
         self.port = port
         self.db = db
         local.db = redis.Redis(host=host, port=port, db=db)
+        self.dispatch = SharedDataMiddleware(self.dispatch, {
+                '/static':  STATIC_PATH
+                })
 
-    def __call__(self, environ, start_response):
+    def dispatch(self, environ, start_response):
         local.application = self
         local.db = redis.Redis(host=self.host, port=self.port, db=self.db)
         request = Request(environ)
@@ -31,3 +36,6 @@ class CadorsFeed(object):
             response = e
         return ClosingIterator(response(environ, start_response),
                                [local_manager.cleanup])
+
+    def __call__(self, environ, start_response):
+        return self.dispatch(environ, start_response)
