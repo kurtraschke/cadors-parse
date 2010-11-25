8002d5ef36d817814b4d47ae55c0f13222764fd5
Kurt Raschke <kurt@kurtraschke.com>
2010-11-25 13:37:29 -0500

    Indentation fixes in manage.py.

--- src/cadorsfeed/manage.py
+++ src/cadorsfeed/manage.py
@@ -1,24 +1,25 @@
 from flaskext.script import Manager, prompt_pass, prompt_bool
 from flask import g
 from functools import wraps
+import redis
 
 from cadorsfeed import create_app
 from cadorsfeed.auth import set_password
 
-import redis
-
 manager = Manager(create_app)
 manager.add_option('-c', '--config', dest='config', required=False)
 
+
 def with_db(f):
-     @wraps(f)
-     def wrapper(*args, **kwargs):
-         from flask import g, current_app as app
-         g.db = redis.Redis(host=app.config['REDIS_HOST'],
-                            port=app.config['REDIS_PORT'],
-                            db=app.config['REDIS_DB'])
-         return f(*args, **kwargs)
-     return wrapper
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        from flask import g, current_app as app
+        g.db = redis.Redis(host=app.config['REDIS_HOST'],
+                        port=app.config['REDIS_PORT'],
+                        db=app.config['REDIS_DB'])
+        return f(*args, **kwargs)
+    return wrapper
+
 
 @manager.option('-u', '--username', dest='username', required=True)
 @with_db
@@ -27,6 +28,7 @@ def adduser(username):
     password = prompt_pass("Enter password for user %s" % username)
     set_password(username, password)
 
+
 @manager.option('-u', '--username', dest='username', required=True)
 @with_db
 def deluser(username):
@@ -34,9 +36,17 @@ def deluser(username):
     if g.db.exists("users:" + username):
         confirm = prompt_bool("Are you sure you wish to delete user %s" % username)
         if confirm:
-            g.db.delete("users:"+username)
+            g.db.delete("users:" + username)
     else:
         print "User %s does not exist." % username
+
+
+@manager.command
+@with_db
+def load():
+    from cadorsfeed.aerodromes import fetch_aerodromes
+    fetch_aerodromes()
+
 
 def run():
     manager.run()
