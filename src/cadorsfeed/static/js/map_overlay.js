0309287e322989e88f1914bcf22e66fa6ade3deb
Kurt Raschke <kurt@kurtraschke.com>
2011-03-29 21:05:58 -0400

    Code cleanup based on application of JSLint.

--- src/cadorsfeed/static/js/map_overlay.js
+++ src/cadorsfeed/static/js/map_overlay.js
@@ -1,56 +1,54 @@
 google.load("earth", "1");
-google.setOnLoadCallback(function() {
-$(document).ready(function() {
-  
-
-  $('div.report').each(
-    function(index, element) {
-
-      var thelink = $('a.maplink', element);
-      var thekml = thelink.attr('href');
-      var theoverlay = $('div.overlay', element);
+google.setOnLoadCallback(function () {
+    $(document).ready(function () {
+        $('div.report').each(
+            function (index, element) {
+                var thelink, thekml, theoverlay;
+                thelink = $('a.maplink', element);
+                thekml = thelink.attr('href');
+                theoverlay = $('div.overlay', element);
  
-      $(thelink).overlay({
-        top: 'center',
-      
-        target: $(theoverlay),
-
-        onLoad: function(event) {
-          var initCB = function (instance) {
-            var ge = instance;
-            ge.getWindow().setVisibility(true);
-            ge.getNavigationControl().setVisibility(ge.VISIBILITY_AUTO);
-            ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);
-            ge.getOptions().setStatusBarVisibility(true);
-            ge.getOptions().setScaleLegendVisibility(true);
+                $(thelink).overlay({
+                    top: 'center',
+                    target: $(theoverlay),
+                    onLoad: function (event) {
+                        var initCB, failureCB, themap;
+                        initCB = function (instance) {
+                            var ge, link, href, networkLink;
+                            ge = instance;
+                            ge.getWindow().setVisibility(true);
+                            ge.getNavigationControl().setVisibility(
+                                ge.VISIBILITY_AUTO);
+                            ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS,
+                                                              true);
+                            ge.getOptions().setStatusBarVisibility(true);
+                            ge.getOptions().setScaleLegendVisibility(true);
             
-            var link = ge.createLink('');
-            var href = thekml;
-            link.setHref(href);
+                            link = ge.createLink('');
+                            href = thekml;
+                            link.setHref(href);
  
-            var networkLink = ge.createNetworkLink('');
-            networkLink.set(link, true, true);
+                            networkLink = ge.createNetworkLink('');
+                            networkLink.set(link, true, true);
  
-            ge.getFeatures().appendChild(networkLink);
-          }
+                            ge.getFeatures().appendChild(networkLink);
+                        };
  
-          var failureCB = function(errorCode) {
-            console.log(errorCode)
-          }
+                        failureCB = function (errorCode) {
+                            console.log(errorCode);
+                        };
 
-          var themap = $('div.map', theoverlay)[0];
+                        themap = $('div.map', theoverlay)[0];
 
-          google.earth.createInstance(themap,
-            initCB, failureCB);
-	},
+                        google.earth.createInstance(themap, initCB, failureCB);
+	                },
 
-        onClose: function(event) {
-          //Clear out the overlay so the plugin goes away.
-          $('div.map', theoverlay)[0].innerHTML = '';
-        }
-
-      });
-    }
-  );
-})
+                    onClose: function (event) {
+                        //Clear out the overlay so the plugin goes away.
+                        $('div.map', theoverlay)[0].innerHTML = '';
+                    }
+                });
+            }
+        );
+    });
 });
