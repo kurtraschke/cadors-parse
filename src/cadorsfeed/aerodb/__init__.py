7a839d420bce630e68f2bb5a488fb9fd5157076b
Kurt Raschke <kurt@kurtraschke.com>
2011-05-17 16:42:10 -0400

    Restore lookup.py, which shouldn't have gotten deleted.

--- src/cadorsfeed/aerodb/__init__.py
+++ src/cadorsfeed/aerodb/__init__.py
@@ -1,3 +1,4 @@
 from cadorsfeed.aerodb.load import import_aerodromes
+from cadorsfeed.aerodb.lookup import lookup
 from cadorsfeed.aerodb.find import aerodromes_re, replace_aerodromes
 from cadorsfeed.aerodb.blacklist import import_blacklist
---
+++ src/cadorsfeed/aerodb/lookup.py
@@ -0,0 +1,12 @@
+from sqlalchemy import or_
+from cadorsfeed.models import Aerodrome
+
+
+def lookup(code):
+    if code is None:
+        return None
+    aerodrome = Aerodrome.query.filter(or_(Aerodrome.icao == code,
+                                           Aerodrome.iata == code,
+                                           Aerodrome.faa == code,
+                                           Aerodrome.tclid == code)).first()
+    return aerodrome
